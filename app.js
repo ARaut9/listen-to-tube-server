d26e5e76b0d4fb0fca5a8f7bf27526b254dff786
araut9 <ankitraut999@gmail.com>
2020-03-23 12:36:20 +0530

    add logic to routes for search, video, and playlist

--- app.js
+++ app.js
@@ -1,22 +1,82 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const ytsr = require('ytsr');
+const ytdl = require('ytdl-core');
+const ytpl = require('ytpl');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(cors());
 
-app.get('/search', (req, res) => {
-  res.json({ name: 'result 1' });
+app.get('/search', (req, res, next) => {
+  // String specifying the query to search for
+  const { q } = req.query;
+
+  /*  * String specifying the type to filter results based on
+      * 'playlist' is the only valid option for now,
+      * in all other cases results are filtered based on video as type  */
+  const { type } = req.query;
+
+  /*  * performs a search based on the search query
+      * and then applies either the video filter
+      * or the playlist filter, if the request contains a type of playlist  */
+  ytsr.getFilters(q, (err, filters) => {
+    if (err) {
+      next(err);
+    } else {
+      const filter = type === 'playlist'
+        ? filters.get('Type').find((o) => o.name === 'Playlist')
+        : filters.get('Type').find((o) => o.name === 'Video');
+
+      const options = {
+        nextpageRef: filter.ref,
+      };
+
+      ytsr(null, options, (err, searchResult) => (err ? next(err) : res.json(searchResult)));
+    }
+  });
 });
 
-app.get('/video', (req, res) => {
-  res.json({ name: 'video name' });
+app.get('/video', (req, res, next) => {
+  const videoURL = req.query.url;
+  const isValidURL = ytdl.validateURL(videoURL);
+
+  if (isValidURL) {
+    const videoID = ytdl.getURLVideoID(videoURL);
+
+    ytdl.getInfo(videoID, (err, info) => {
+      if (err) {
+        next(err);
+      } else {
+        const { videoDetails } = info.player_response;
+        // filter the formats array to only include audio formats
+        const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
+        const relatedVideos = info.related_videos;
+
+        res.json({
+          videoDetails,
+          audioFormats,
+          relatedVideos,
+        });
+      }
+    });
+  } else {
+    next(Error('Invalid URL!'));
+  }
 });
 
-app.get('/playlist', (req, res) => {
-  res.json({ name: 'playlist name' });
+app.get('/playlist', (req, res, next) => {
+  const playlistURL = req.query.url;
+  const isValidURL = ytpl.validateURL(playlistURL);
+
+  if (isValidURL) {
+    const playlistID = ytpl.getPlaylistID(playlistURL, (err, id) => (err ? next(err) : id));
+    ytpl(playlistID, (err, result) => (err ? next(err) : res.json(result)));
+  } else {
+    next(Error('Invalid URL!'));
+  }
 });
 
 // unknown endpoint handler
